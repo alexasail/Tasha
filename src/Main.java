e9d2695edc8de06605030566c2aff484f465f85a
alexasail <112632305+alexasail@users.noreply.github.com>
2023-02-09 22:21:36 +0300

    fileSuperPuper

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,14 @@
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Game game = new Game();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("1. Играть\n2. Раздел статистики\n3. Завершить игру\n");
-            int move = scanner.nextInt();
-            if (move == 1) {
-                game.game();
-            }
-            if (move == 2) {
-                while (true) {
-                    System.out.println("\n1.Статистика по победам.");
-                    System.out.println("2.Статистика по поражениям.");
-                    System.out.println("3.Статистика по кол-ву игр.");
-                    System.out.println("4.Статистика по победам подряд.");
-                    System.out.println("5.Ачивки.");
-                    System.out.println("6.Выйти.\n");
-                    int move2 = scanner.nextInt();
-                    System.out.println();
-                    if (move2 == 1) {
-                        game.printWins();
-                    }
-                    if (move2 == 2) {
-                        game.printLosses();
-                    }
-                    if (move2 == 3) {
-                        game.setGames();
-                    }
-                    if (move2 == 4) {
-                        game.setWinsInARow();
-                    }
-                    if (move2 == 5) {
-                        game.setAchievementsForGame();
-                    }
-                    if (move2 == 6) {
-                        break;
-                    }
-                }
-            }
-            if (move == 3) {
-                game.printWins();
-                game.printLosses();
-                game.setGames();
-                game.setWinsInARow();
-                game.setAchievementsForGame();
-                break;
-            }
+        DirLister dirLister = new DirLister();
+        List<FileEntry> finalFile = dirLister.listDir(Path.of("/Users/aleksefremov/Desktop/photo/for_seting"));
+        for (FileEntry fileEntry : finalFile) {
+            System.out.println(fileEntry);
         }
     }
+
 }
